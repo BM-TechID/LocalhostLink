ca8e1174c049f5bca709506bdec86a0719fdb4cc
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-11-20 09:19:38 +0700

    Update link.sh

--- link.sh
+++ link.sh
@@ -8,8 +8,8 @@ rm -rf /.ssh/known_hosts
 screen -dmS remote -L -Logfile logremote.txt ssh -R 80:localhost:8$
 clear
 echo "Tunggu dela ya..."; sleep 2
-echo "Tek ngirim link meng telegram..."; sleep 10
 sh bot.sh
+echo "Tek ngirim link meng telegram..."; sleep 10
 host=$(cat logremote.txt|grep tunneled|awk '{print $1}')
 echo ""
 echo "Your Link: $host"
