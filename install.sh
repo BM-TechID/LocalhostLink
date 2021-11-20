bec7aaa460eb1c35d81682eb44e59741c6045b49
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-11-20 09:14:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "===================================="
+echo "Installer remote Localhost to Publik"
+echo "===================================="
+
+apt update && apt install openssh-server screen wget
+sleep 2; clear
