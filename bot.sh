f057c4ccbcd45d251effc189921373d3f6838cc8
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-11-20 09:45:47 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Bot telegram
 
-TOKEN_BOT="2105823821:AAFczmXdo52u94usWbMVqWCAq9A-oCzPHHM"
-CHAT_ID="1178201791"
+TOKEN_BOT=""
+CHAT_ID=""
 
+clear
 echo "Cek Token Bot kamu..."; sleep 1
 echo "$TOKEN_BOT"
 echo "Cek juga id telegramu...."; sleep 1
 echo "$CHAT_ID"
-sleep 1; echo "Oke siap"
+sleep 1; echo "Oke siap"; clear
 
 host=$(cat logremote.txt|grep tunneled|awk '{print $1}')
 PESAN="Id server: <b>$(hostname)</b>%0ALink: $host"
