189f327a8546af9412f21eac88c26baaea87eb66
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-11-20 09:57:21 +0700

    Update link.sh

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@ echo "Enteni dela cuk agi di setting...."; sleep 1; echo ""
 killall -q screen
 rm -rf logremote.txt 
 rm -rf /.ssh/known_hosts
-screen -dmS remote -L -Logfile logremote.txt ssh -R 80:localhost:8080 nokey@localhost.run
+screen -dmS remote -L -Logfile logremote.txt ssh -R 80:localhost:80 nokey@localhost.run
 clear
 echo "Tunggu dela ya..."; sleep 2
 echo "Tek ngirim link meng telegram..."; sleep 10
